7fd4bace3daa0d7ef1d94ee32747bbde519f7a0c
Anatoliy Yermakov <70053381+i7aket@users.noreply.github.com>
2024-04-24 20:50:40 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// мобільне меню
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+const menuClose = document.querySelector('.menu__close');
+
+menuBtn.addEventListener('click', ()=>{
+    menuList.classList.toggle('menu--open');
+});
+menuClose.addEventListener('click', ()=>{
+    menuList.classList.remove('menu--open');
+});
+
+//закриття бургера після кліку на посилання
+$('.menu__btn').on('click', function () {
+	$('.menu__list').addClass('menu--open');
+  });
+  
+  $( '.menu__link' ).on("click", function(){
+	$('.menu__list').removeClass('menu--open');
+  });
+
+
+  $('services__item-img').each(function(fadeInImg) {
+    $(this).delay(fadeInImg * 50).fadeIn(100);
+  });
+
+
+
+  
+    
+    
